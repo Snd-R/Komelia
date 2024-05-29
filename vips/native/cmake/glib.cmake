6011a26e20e24b5bb4eba167cd7f9acb61c0e3f2
Snd-R <76580768+snd-r@users.noreply.github.com>
2024-05-29 20:28:14 +0300

    glib2 version update for vips build

--- vips/native/cmake/glib.cmake
+++ vips/native/cmake/glib.cmake
@@ -7,10 +7,8 @@ endif ()
 
 list(APPEND DEPENDENCIES ep_glib)
 ExternalProject_Add(ep_glib
-        #URL https://download.gnome.org/sources/glib/2.80/glib-2.80.0.tar.xz
-        #URL_HASH SHA256=8228a92f92a412160b139ae68b6345bd28f24434a7b5af150ebe21ff587a561d
-        URL https://download.gnome.org/sources/glib/2.78/glib-2.78.4.tar.xz
-        URL_HASH SHA256=24b8e0672dca120cc32d394bccb85844e732e04fe75d18bb0573b2dbc7548f63
+        URL https://download.gnome.org/sources/glib/2.80/glib-2.80.2.tar.xz
+        URL_HASH SHA256=b9cfb6f7a5bd5b31238fd5d56df226b2dda5ea37611475bf89f6a0f9400fe8bd
         CONFIGURE_COMMAND
         PKG_CONFIG_PATH=${THIRD_PARTY_LIB_PATH}/lib/pkgconfig PKG_CONFIG_PATH_CUSTOM=${THIRD_PARTY_LIB_PATH}/lib/pkgconfig LD_LIBRARY_PATH=${THIRD_PARTY_LIB_PATH}/lib:$ENV{LD_LIBRARY_PATH} ${ENV_PATHS}
         ${Meson_EXECUTABLE} setup -D selinux=disabled -D glib_debug=disabled ${MESON_CROSS_FILE_ARG} --prefix=<INSTALL_DIR> --libdir=lib
