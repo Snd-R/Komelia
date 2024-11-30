9156bb6adaf5607447fc2fecab1d8e97d47d7856
Snd-R <76580768+snd-r@users.noreply.github.com>
2024-11-30 15:36:23 +0300

    version 0.12.0

--- komelia-core/src/commonMain/kotlin/io/github/snd_r/komelia/updates/AppVersion.kt
+++ komelia-core/src/commonMain/kotlin/io/github/snd_r/komelia/updates/AppVersion.kt
@@ -17,7 +17,7 @@ data class AppVersion(
 ) : Comparable<AppVersion> {
 
     companion object {
-        val current = AppVersion(0, 11, 1)
+        val current = AppVersion(0, 12, 0)
 
         fun fromString(value: String): AppVersion {
             val version = value.split(".")
