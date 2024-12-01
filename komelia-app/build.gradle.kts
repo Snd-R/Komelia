0a91d0a0ecd2b26ba5867e358657f04fa476dc0d
Snd-R <76580768+snd-r@users.noreply.github.com>
2024-12-01 09:04:00 +0300

    set android version code directly

--- komelia-app/build.gradle.kts
+++ komelia-app/build.gradle.kts
@@ -83,7 +83,7 @@ android {
         applicationId = "io.github.snd_r.komelia"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = libs.versions.android.versionCode.get().toInt()
+        versionCode = 3
         versionName = libs.versions.app.version.get()
     }
     packaging {
