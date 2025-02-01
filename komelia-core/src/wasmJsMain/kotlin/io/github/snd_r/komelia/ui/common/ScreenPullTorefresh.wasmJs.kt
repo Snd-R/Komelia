6c213f487542151606c0149c5cd7f5dc4ad55ef5
Snd-R <76580768+snd-r@users.noreply.github.com>
2025-02-01 12:21:41 +0300

    use pull to refresh on Android only

--- komelia-core/src/wasmJsMain/kotlin/io/github/snd_r/komelia/ui/common/ScreenPullTorefresh.wasmJs.kt
+++ komelia-core/src/wasmJsMain/kotlin/io/github/snd_r/komelia/ui/common/ScreenPullTorefresh.wasmJs.kt
@@ -1,14 +1,15 @@
 package io.github.snd_r.komelia.ui.common
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.runtime.Composable
 import io.github.snd_r.komelia.ui.LoadState
 import kotlinx.coroutines.flow.Flow
 
 @Composable
-expect fun ScreenPullToRefreshBox(
+actual fun ScreenPullToRefreshBox(
     screenState: Flow<LoadState<*>>,
     onRefresh: () -> Unit,
-    minLoadDuration: Long = 700,
+    minLoadDuration: Long,
     content: @Composable BoxScope.() -> Unit,
-)
+) = Box(content = content)
