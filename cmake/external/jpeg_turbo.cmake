1b8fb4d85685180bf4a08de914c1691d01d2532b
Snd-R <76580768+snd-r@users.noreply.github.com>
2026-08-15 13:06:31 +0300

    disable libjpeg-turbo api

--- cmake/external/jpeg_turbo.cmake
+++ cmake/external/jpeg_turbo.cmake
@@ -4,7 +4,10 @@ ExternalProject_Add(ep_jpeg-turbo
         SOURCE_DIR ${THIRD_PARTY_SOURCE_PATH}/libjpeg-turbo
         CMAKE_ARGS
             ${EP_CMAKE_ARGS}
-            -DWITH_SYSTEM_ZLIB=ON
+            -DWITH_TOOLS=OFF
+            -DWITH_TESTS=OFF
+            -DWITH_TURBOJPEG=OFF
+            -DWITH_JNA=OFF
         USES_TERMINAL_DOWNLOAD true
         USES_TERMINAL_BUILD true
 )
